cfe33c32ec98c171cc09c5e7582acb711533ba97
Ahmed-Mujahed <133661163+Ahmed-Mujahed@users.noreply.github.com>
2023-05-15 15:20:40 +0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,6 @@
+console.log("Welcome to What's on main web");
+let age =  window.prompt("Enter your age people under 18 are not allowed")
+if(age < 18){
+    window.alert("you are not allowed");
+    window.close();
+}
